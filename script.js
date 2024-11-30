f8a24937bcb531ca218d63770009b16acb32feb7
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-11-29 21:56:49 -0500

    creating variabkes to link ID from html to javascript file

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const myForm = document.getElementById('todo-form');
+const taskInput = document.getElementById('task-input');
+const taskSubmit = document.getElementById('submit_task');
+const showTasks = document.getElementById('all_tasks');
+const status = document.getElementById('option');
+
