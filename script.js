3f37476d7fb9470e51d469340a7a731097478693
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-11-29 22:11:12 -0500

    reverted the html file back to original

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const myForm = document.getElementById('todo-form');
 const taskInput = document.getElementById('task-input');
-const taskSubmit = document.getElementById('submit_task');
+const taskSubmit = document.getElementById('task-submit');
 const showTasks = document.getElementById('all_tasks');
 const status = document.getElementById('option');
+const edit = document.getElementById('edit');
+const deleteButton = document.getElementById('delete');
 
