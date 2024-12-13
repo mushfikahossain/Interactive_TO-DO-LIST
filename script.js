498ecd97374974f644a4e0975d9f83bd465b70b5
Auckloo Veevek <avish@Auckloos-MacBook-Pro.local>
2024-12-13 15:44:29 -0500

    added new error-message to show if user attemps to create blanck tasks

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const myForm = document.getElementById('todo-form');
 const taskInput = document.getElementById('task-input');
 const taskSubmit = document.getElementById('task-submit');
-const showTasks = document.getElementById('all_tasks');
+const showTasks = document.getElementById('tasks');
 const status = document.getElementById('option');
 const edit = document.getElementById('edit');
 const deleteButton = document.getElementById('delete');
+const errorMessage = document.getElementById('error-message'); 
 
+
+myForm.addEventListener('submit', function (prevent) {
+    prevent.preventDefault(); 
+
+    
+    if (taskInput.value.trim() === '') {
+        errorMessage.style.display = 'block';
+        return; 
+    }
+    errorMessage.style.display = 'none';
+
+    console.log('Task added:', taskInput.value);
+
+    taskInput.value = '';
+});
